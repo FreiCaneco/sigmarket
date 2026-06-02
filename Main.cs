46818eba4e22fb52b60e607481a515831d9fa67d
Matheus CR <matheuscreinaldo07@gmail.com>
2026-06-02 17:16:50 -0300

    Fix: Caminho do computer_screen dentro do script principal. Feature: Início do desenvolvimento dos shaders

--- Main.cs
+++ Main.cs
@@ -6,54 +6,54 @@ namespace sigmarket;
 
 public partial class Main : Node2D
 {
-    private SubViewport _subViewport;
-    private CanvasLayer _canvasLayer;
-    
-    private PackedScene _gameScene = GD.Load<PackedScene>("res://Scenes/game.tscn");
-    private PackedScene _computerMenuScene = GD.Load<PackedScene>("res://Ui/computer_screen.tscn");
+	private SubViewport _subViewport;
+	private CanvasLayer _canvasLayer;
+	
+	private PackedScene _gameScene = GD.Load<PackedScene>("res://Scenes/game.tscn");
+	private PackedScene _computerMenuScene = GD.Load<PackedScene>("res://UI/ItemShop/computer_screen.tscn");
 
-    [Export] public MainMenu MainMenu;
+	[Export] public MainMenu MainMenu;
 
-    public override void _Ready()
-    {
-        _subViewport = GetNode<SubViewport>("160x90Viewport/SubViewport");
-        _canvasLayer = GetNode<CanvasLayer>("CanvasLayer");
-        
-        SignalBus.Instance.ComputerInteracted += OnComputerInteracted;
-        MainMenu.PlayPressed += OnMainMenuPlayPressed;
-    }
+	public override void _Ready()
+	{
+		_subViewport = GetNode<SubViewport>("160x90Viewport/SubViewport");
+		_canvasLayer = GetNode<CanvasLayer>("CanvasLayer");
+		
+		SignalBus.Instance.ComputerInteracted += OnComputerInteracted;
+		MainMenu.PlayPressed += OnMainMenuPlayPressed;
+	}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("exit") && _canvasLayer.Visible) ExitComputer();
-    }
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("exit") && _canvasLayer.Visible) ExitComputer();
+	}
 
-    private void OnMainMenuPlayPressed()
-    {
-        var gameScene = _gameScene.Instantiate();
-        var computerMenuScene = (Control)_computerMenuScene.Instantiate();
+	private void OnMainMenuPlayPressed()
+	{
+		var gameScene = _gameScene.Instantiate();
+		var computerMenuScene = (Control)_computerMenuScene.Instantiate();
 
-        _subViewport.GetChild(0).QueueFree();
-        _subViewport.AddChild(gameScene);
-        
-        _canvasLayer.AddChild(computerMenuScene);
-        computerMenuScene.Visible = false;
-    }
+		_subViewport.GetChild(0).QueueFree();
+		_subViewport.AddChild(gameScene);
+		
+		_canvasLayer.AddChild(computerMenuScene);
+		computerMenuScene.Visible = false;
+	}
 
-    private void OnComputerInteracted()
-    {
-        GetTree().Paused = true;
-        _canvasLayer.Show();
-    }
+	private void OnComputerInteracted()
+	{
+		GetTree().Paused = true;
+		_canvasLayer.Show();
+	}
 
-    private void ExitComputer()
-    {
-        GetTree().Paused = false;
-        _canvasLayer.Hide();
-    }
+	private void ExitComputer()
+	{
+		GetTree().Paused = false;
+		_canvasLayer.Hide();
+	}
 
-    public override void _ExitTree()
-    {
-        SignalBus.Instance.ComputerInteracted -= OnComputerInteracted;
-    }
+	public override void _ExitTree()
+	{
+		SignalBus.Instance.ComputerInteracted -= OnComputerInteracted;
+	}
 }
